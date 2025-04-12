ef3ae74aeb7045c7ad193bab1ed8217a565835fa
Scott Green <skevingreen@gmail.com>
2025-04-12 12:13:23 -0700

    completed hands-on 4.1

--- data-files/rpg-character-builder/src/app/create-character/create-character.component.spec.ts
+++ data-files/rpg-character-builder/src/app/create-character/create-character.component.spec.ts
@@ -6,6 +6,7 @@ describe('CreateCharacterComponent', () => {
   let component: CreateCharacterComponent;
   let fixture: ComponentFixture<CreateCharacterComponent>;
 
+  // setup for each test
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CreateCharacterComponent]
@@ -17,7 +18,43 @@ describe('CreateCharacterComponent', () => {
     fixture.detectChanges();
   });
 
+  // default test to make sure component was created
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // make sure that when a character is added that the corresponding id is generated correctly (a number between 1 and 1000)
+  it('should generate a random character ID between 1 and 1000 with no decimal places', () => {
+    component.addToCharacterList();
+    expect(component.characters[0].id).toBeGreaterThan(0);
+    expect(component.characters[0].id).toBeLessThan(1000);
+    expect(Number.isInteger(component.characters[0].id)).toBe(true);
+  });
+
+  // customize a character, add it to the characters array, and check that after being added it has the expected values
+  it('should add a character with correct customization', () => {
+    component.name = "Hope";
+    component.selectedGenderId = 3;
+    component.selectedClassId = 3;
+    component.addToCharacterList();
+
+    const addedCharacter = component.characters[0];
+
+    expect(addedCharacter.name).toBe("Hope");
+    expect(addedCharacter.gender).toBe("Other");
+    expect(addedCharacter.characterClass).toBe("Rogue");
+  });
+
+  // check that resetting the form puts teh form values back to default
+  it('should reset all fields to their default values after resetForm is called', () => {
+    component.name = "Ted";
+    component.selectedGenderId = 2;
+    component.selectedClassId = 2;
+
+    component.resetForm();
+
+    expect(component.name).toBe("");
+    expect(component.selectedGenderId).toBe(1);
+    expect(component.selectedClassId).toBe(1);
   });
 });
--- data-files/rpg-character-builder/src/app/create-character/create-character.component.ts
+++ data-files/rpg-character-builder/src/app/create-character/create-character.component.ts
@@ -1,16 +1,190 @@
+// interface to define Character properties
+export interface Character {
+  id: number;
+  name: string;
+  gender: string;
+  characterClass: string;
+}
+
+// interface to define Gender properties
+export interface Gender {
+  id: number;
+  description: string;
+}
+
+// interface to define Class properties
+export interface Class {
+  id: number;
+  description: string;
+}
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-character',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   template: `
-    <p>
-      create-character works!
-    </p>
+    <div class="character-form-container">
+      <form class="character-form" #characterCreationForm="ngForm" (ngSubmit)="addToCharacterList();">
+        <h1>Complete the form below to create a new character.</h1>
+        <fieldset>
+          <legend>My Character</legend>
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" class="name-input" [(ngModel)]="name" ngModel>
+
+          <label for="gender">Gender</label>
+          <select name="gender" id="gender" [(ngModel)]="selectedGenderId" ngModel>
+            @for(gender of genders; track gender) {
+              <option value="{{gender.id}}">{{gender.description}}</option>
+            }
+          </select>
+
+          <label for="characterClass">Character Class</label>
+          <select name="characterClass" id="characterClass" [(ngModel)]="selectedClassId" ngModel>
+            @for(aClass of classes; track aClass) {
+              <option value="{{aClass.id}}">{{aClass.description}}</option>
+            }
+          </select>
+
+          <input type="submit" value="Submit" />
+        </fieldset>
+      </form>
+
+      <div class="character-list-summary">
+        <h1>Character List Summary</h1>
+
+        @if(characters.length > 0) {
+          <ul>
+            @for(character of characters; track character) {
+              <li>
+                <em>Name: </em>{{character.name}}
+                <em>Gender: </em>{{character.gender}}
+                <em>Class: </em>{{character.characterClass}}
+                <br />
+              </li>
+            }
+          </ul>
+        } @else {
+          <p>No characters added to the list yet.</p>
+        }
+      </div>
+    </div>
   `,
-  styles: ``
+  styles: [
+    `
+      .character-form-container {
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;
+      }
+
+      .character-form {
+        flex: 1;
+        margin-right: 20px;
+      }
+
+      .character-list-summary {
+        flex: 1;
+      }
+
+      fieldset {
+        margin-bottom: 20px;
+      }
+
+      label, select, .name-input {
+        display: block;
+        margin-bottom: 5px;
+      }
+
+      .name-input, select, input[type="submit"] {
+        padding: 8px;
+        box-sizing: border-box;
+      }
+
+      select {
+        width: 100%
+      }
+
+      .qty-input {
+        width: 20%;
+      }
+
+      input[type="submit"] {
+        float: right;
+      }
+
+      .customization-option {
+        display: flex;
+        align-items: center;
+        margin-bottom: 5px;
+      }
+
+      .character-list-summary li {
+        margin-bottom: 10px;
+        padding: 5px;
+      }
+    `
+  ]
 })
 export class CreateCharacterComponent {
+  genders: Gender[];
+  classes: Class[];
+  selectedGenderId: number;
+  selectedClassId: number;
+  characters: Character[];
+  name: string;
 
+  constructor() {
+    this.genders = [                  // populate a list of genders for the form selector
+      {id:1, description:"Male"},
+      {id:2, description:"Female"},
+      {id:3, description:"Other"}
+    ];
+
+    this.classes = [                  // populate a list of classes for the form selector
+      {id:1, description:"Warrior"},
+      {id:2, description:"Mage"},
+      {id:3, description:"Rogue"}
+    ]
+
+    this.selectedGenderId = this.genders[0].id; // initialize the selected gender id
+    this.selectedClassId = this.classes[0].id;  // initialize the selected class id
+    this.characters = [];                       // initialize the characters array to be empty
+    this.name = "";                             // initialize the character name to an empty string
+  }
+
+  // adds a generated character to the characters array
+  addToCharacterList() {
+    const selectedGenderNum = Number(this.selectedGenderId);                              // get the index of the selected gender
+    const selectedGender = this.genders.find(gender => gender.id === selectedGenderNum);  // get the selected gender object
+
+    const selectedClassNum = Number(this.selectedClassId);                                // get the index of the selected class
+    const selectedClass = this.classes.find(aClass => aClass.id === selectedClassNum)     // get the selected class object
+
+    if (selectedGender !== undefined && selectedClass != undefined) {   // check that both gender and class were defined
+      const characterToAdd = {                                          // create the character
+        id:  Math.floor(Math.random() * 1000) + 1,                      // set the character id to a random number between 1 and 1000
+        name: this.name,                                                // set the name to the text entered in the form
+        gender: selectedGender.description,                             // set the gender based on form selection
+        characterClass: selectedClass.description                       // set the class based on form selection
+      }
+
+      this.characters.push(characterToAdd);                             // put the created character on the characters array
+      console.log('Character List after adding: ', this.characters);    // log the characters list after adding the new character
+
+      this.resetForm();   // reset the form to defaults so more characters can be created
+    } else {
+      console.error('Either gender or class was undefined.'); // log gender/class related error
+    }
+  }
+
+  // resets the form to default values
+  resetForm() {
+    this.selectedGenderId = this.genders[0].id;   // reset gender form selection to default
+    this.selectedClassId = this.classes[0].id;    // reset class form selection to default
+    this.name = "";                               // reset name form entry to default
+  }
 }
