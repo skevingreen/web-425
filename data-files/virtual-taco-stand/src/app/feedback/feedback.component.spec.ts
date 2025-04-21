1596317095aecf6be9ebee3711032c8490b5887a
Scott Green <skevingreen@gmail.com>
2025-04-20 17:06:13 -0700

    completed chapter 6 exercise

--- data-files/virtual-taco-stand/src/app/feedback/feedback.component.spec.ts
+++ data-files/virtual-taco-stand/src/app/feedback/feedback.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FeedbackComponent } from './feedback.component';
+import { By } from '@angular/platform-browser';
 
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
@@ -8,7 +9,7 @@ describe('FeedbackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FeedbackComponent]
+      imports: [FeedbackComponent, ReactiveFormsModule, FormsModule]
     })
     .compileComponents();
 
@@ -19,5 +20,31 @@ describe('FeedbackComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form should be invalid when empty', () => {
+    expect(component.feedbackForm.valid).toBeFalsy();
+  });
+
+  it('form should be valid when filled correctly', () => {
+    component.feedbackForm.controls['rating'].setValue('5');
+    component.feedbackForm.controls['recommend'].setValue('Yes');
+    component.feedbackForm.controls['comments'].setValue('Great service!');
+
+    expect(component.feedbackForm.valid).toBeTruthy();
+  });
+
+  it('should call leaveFeedback on form submit with valid data', () => {
+    spyOn(component, 'leaveFeedback');
+
+    component.feedbackForm.controls['rating'].setValue('5');
+    component.feedbackForm.controls['recommend'].setValue('Yes');
+    component.feedbackForm.controls['comments'].setValue('Great service!');
+    fixture.detectChanges();
+
+    const form = fixture.debugElement.query(By.css('form'));
+    form.triggerEventHandler('ngSubmit', null);
+
+    expect(component.leaveFeedback).toHaveBeenCalled();
   });
 });
